e7f3351bd3e62ea06d6e17939e32d70e3984e807
MaxibX <114149183+MaxibX@users.noreply.github.com>
2023-05-20 02:39:50 +0700

    v5

--- lab1/src/main/java/Sources.java
+++ lab1/src/main/java/Sources.java
@@ -1,7 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import presenter.Presenter;
 
 public class Sources {
+    private static Logger logger = LogManager.getLogger(Sources.class);
     public static void main(String[] args) {
+        logger.info("Start logging");
         Presenter presenter = new Presenter();
         presenter.run();
     }
